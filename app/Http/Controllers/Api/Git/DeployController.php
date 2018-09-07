a764186b24969af8eba3347ca2013b4b0375cce2
irooit <hi@rooit.com>
2018-09-07 12:08:35 +0800

    [add] gitcontroller

--- app/Http/Controllers/Api/Git/DeployController.php
+++ app/Http/Controllers/Api/Git/DeployController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Git;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 
 class DeployController extends Controller
 {
@@ -12,6 +13,8 @@ class DeployController extends Controller
 
     public function __invoke(Request $request)
     {
+        $all = $request->all();
+        Log::info('gitee', $all);
         return $this->response->array($request->all());
     }
 }
--- app/Http/Controllers/Gitcontroller.php
+++ app/Http/Controllers/Gitcontroller.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class Gitcontroller extends Controller
 {
-    /*public function __invoke(Request $request)
-    {
-        return '123';
-    }*/
 
     public function index()
     {
