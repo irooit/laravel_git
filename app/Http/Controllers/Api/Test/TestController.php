4ef1b7b3b67de16ea4b0ac426d89ff1ba9753a59
irooit <hi@rooit.com>
2018-09-08 23:02:44 +0800

    [add] gitcontroller

--- app/Http/Controllers/Api/Test/TestController.php
+++ app/Http/Controllers/Api/Test/TestController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\api\Test;
 
+use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
+    use Helpers;
     public function index()
     {
-        
+        $data = @file_get_contents('https://weixin.feisudu.com/addons/rooit_mission/template/style/error.png');
+        return $this->response->item($data);
     }
 }
