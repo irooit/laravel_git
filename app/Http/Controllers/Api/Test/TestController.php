873701ccd52b777c13cb82bea807c74520748db3
irooit <hi@rooit.com>
2018-09-08 23:16:50 +0800

    [add] Testcontroller

--- app/Http/Controllers/Api/Test/TestController.php
+++ app/Http/Controllers/Api/Test/TestController.php
@@ -12,8 +12,10 @@ class TestController extends Controller
     public function index()
     {
         $data = @file_get_contents('https://weixin.feisudu.com/addons/rooit_mission/template/style/error.png');
+        $data1 = @file_get_contents('https://weixin.feisudu.com/addons/rooit_mission/template/image/money.png');
         echo "<pre>";
         var_dump($data);
+        var_dump($data1);
         return $this->response->item([]);
     }
 }
