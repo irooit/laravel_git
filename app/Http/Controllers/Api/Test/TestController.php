6a47c9748dbfa31b97acf07d7f732adf01defdf5
irooit <hi@rooit.com>
2018-09-09 10:07:18 +0800

    [add] Testcontroller

--- app/Http/Controllers/Api/Test/TestController.php
+++ app/Http/Controllers/Api/Test/TestController.php
@@ -12,7 +12,7 @@ class TestController extends Controller
     public function index()
     {
         $data = @file_get_contents('https://weixin.feisudu.com/addons/rooit_mission/template/style/error.png');
-        $data1 = @file_get_contents('https://weixin.feisudu.com/addons/rooit_mission/template/image/money.png');
+        $data1 = @file_get_contents('https://weixin.feisudu.com/addons/rooit_mission/template/style/newbtn.png');
         echo "<pre>";
         var_dump($data);
         var_dump($data1);
